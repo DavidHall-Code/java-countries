0675363d8353d6f8cec1f1c7737554c70637c2e3
DavidHall-Code <david_m_hall@outlook.com>
2021-02-11 20:41:57 -0500

    Set up have some errors

---
+++ src/main/java/com/lambdaschool/countrysearch/repositories/CountryRepository.java
@@ -0,0 +1,6 @@
+package com.lambdaschool.countrysearch.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CountryRepository extends CrudRepository<Country, Long> {
+}
